93e3dd2d01a03783fe51b70c412233a883658f3e
AndregSouza <andregsouzaweb@gmail.com>
2024-02-19 11:21:26 -0300

    Exercise 1.6 Checkpoint

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,66 +1,43 @@
-const Header = (props) => {
-  return(
-    <h1>
-      {props.course}
-    </h1>
-  ) 
-}
-
-const Content = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <Parts part = {props.parts[0].name} exercises = {props.parts[0].exercises} />
-      <Parts part = {props.parts[1].name} exercises = {props.parts[1].exercises} />
-      <Parts part = {props.parts[2].name} exercises = {props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Parts = (props) => {
-  console.log(props.part)
-  return(
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <>
-      <p>
-        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-      </p>
-    </>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { 
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using propos to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseByOne = () => setGood(good + 1)
+  const increaseByOne1 = () => setNeutral(neutral + 1)
+  const increaseByOne2 = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course = {course.name} />
-      <Content parts = {course.parts} />
-      <Total parts = {course.parts} />
+      <Title text = 'give feedback'/>
+      <button onClick={increaseByOne}> good </button>
+      <button onClick={increaseByOne1}> neutral </button>
+      <button onClick={increaseByOne2}> bad </button>
+      <Title text = 'statistics'/>
+      <StatisticLine text = 'good' value = {good}/>
+      <StatisticLine text = 'neutral' value = {neutral}/>
+      <StatisticLine text = 'bad' value = {bad}/>
     </div>
+  )
+}
+
+const Title = ({text}) => {
+  return(
+    <h1>
+      {text}
+    </h1>
+  )
+}
+
+const StatisticLine = (props) =>{
+  return(
+    <p>
+      {props.text} {props.value}
+    </p>
   )
 }
 
