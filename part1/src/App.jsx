426aa646fcbffc0bf29859a5505f2201ad110c34
AndregSouza <andregsouzaweb@gmail.com>
2024-02-19 13:20:05 -0300

    Exercise 1.9 checkpoint

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,13 +6,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
+  const average = (good - bad) / (all.length)
+  const positive = (good / (all.length)) * 100
 
-  const increaseByOne = () => setGood(good + 1)
-  const increaseByOne1 = () => setNeutral(neutral + 1)
-  const increaseByOne2 = () => setBad(bad + 1)
-  const all = good + neutral + bad
-  const average = (good - bad)/all
-  const positive = (good / all) * 100 + ' %'
+  const increaseByOne = () => {
+    setGood(good + 1)
+    setAll(all.concat(1))
+  }
+
+  const increaseByOne1 = () => {
+    setNeutral(neutral + 1)
+    setAll(all.concat(1))
+  }
+  const increaseByOne2 = () => {
+    setBad(bad + 1)
+    setAll(all.concat(1))
+  }
 
   return (
     <div>
@@ -21,12 +31,7 @@ const App = () => {
       <button onClick={increaseByOne1}> neutral </button>
       <button onClick={increaseByOne2}> bad </button>
       <Title text = 'statistics'/>
-      <StatisticLine text = 'good' value = {good}/>
-      <StatisticLine text = 'neutral' value = {neutral}/>
-      <StatisticLine text = 'bad' value = {bad}/>
-      <StatisticLine text = 'all' value = {all}/>
-      <StatisticLine text = 'average' value = {average}/>
-      <StatisticLine text = 'positive' value = {positive} />
+      <Statistics goodCount = {good} neutralCount = {neutral} badCount = {bad} allCount = {all} averageCount = {average} positiveCount = {positive} />
     </div>
   )
 }
@@ -36,6 +41,29 @@ const Title = ({text}) => {
     <h1>
       {text}
     </h1>
+  )
+}
+
+const Statistics = (props) => {
+  console.log(props)
+  
+  if(props.goodCount == 0 && props.neutralCount == 0 && props.badCount == 0){
+    return(
+      <p>
+        No feedbacks given
+      </p>
+    )
+  }
+  
+  return(
+    <div>
+      <StatisticLine text = 'good' value = {props.goodCount}/>
+      <StatisticLine text = 'neutral' value = {props.neutralCount}/>
+      <StatisticLine text = 'bad' value = {props.badCount}/>
+      <StatisticLine text = 'all' value = {props.allCount.length}/>
+      <StatisticLine text = 'average' value = {props.averageCount}/>
+      <StatisticLine text = 'positive' value = {props.positiveCount} />
+    </div>
   )
 }
 
