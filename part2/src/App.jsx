a8a4d6f5ec7f6dad938c3844d3543e346c594218
AndregSouza <andregsouzaweb@gmail.com>
2024-02-21 16:08:21 -0300

    Exercise 2.4 Checkpoint

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,43 +1,70 @@
-const Course = (props) => {
-  console.log(props.course.parts);
-  const initialValue = 0
+const ListCourses = ({props}) => {
   return(
     <>
-      <h1>{props.course.name}</h1>
-      <ul>
-        {props.course.parts.map(content => <li key = {content.id}> {content.name} {content.exercises}</li>)}
-        {props.course.parts.reduce((sum, course) => sum + course.exercises, 0)
-        } 
-      </ul>
+      {props.map(function(course){
+        return (
+          <>
+            <h1 key = {course.id}>{course.name}</h1>
+            <ul> 
+            {course.parts.map(list => <li key = {list.id}> {list.name} </li>)}
+            </ul>
+          </>
+        )
+      })}
     </>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return(
-    <Course course={course} />
+  return (
+    <div>
+      <ListCourses props = {courses} />
+    </div>
   )
 }
 
