21cc31ad4f963c6a2685014eaa236bead9992205
AndregSouza <andregsouzaweb@gmail.com>
2024-02-22 14:45:28 -0300

    Exercise 2.9 checkpoint

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '123456'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,6 +27,7 @@ const App = () => {
       const personObject = {name: newName, number: newNumber}
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -39,9 +41,31 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange =(event) =>{
+    console.log(event.target.value);
+    setNewFilter(event.target.value)
+    // needs an updater function because React states asynchronously)
+    console.log(newFilter);
+    setPersons(persons.filter(function(object){
+      console.log(object);
+      return object.name.includes(event.target.value)
+    }))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with 
+          <input 
+            value ={newFilter}
+            onChange = {handleFilterChange}
+          />
+        </div>
+      </form>
+      
+      <h2>add a new</h2>
       <form>
         <div>
           name: 
@@ -62,10 +86,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(function(prop){
+      {persons.map(function(prop,i){
         return(
           <>
-            <li>{prop.name} , {prop.number} </li>
+            <li key = {i} >{prop.name} / {prop.number} </li>
           </>
         )
       })}
